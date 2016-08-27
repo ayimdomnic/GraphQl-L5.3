16f48dcf8927f6e740199e36fb3828b850d87168
Ayimdomnic <Ayimdomnic@statuscreative.co.ke>
2016-08-27 05:15:20 -0700

    Error handling

---
+++ src/Ayimdomnic/GraphQl/Exceptions/ValidationError.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ayimdomnic
+ * Date: 8/27/2016
+ * Time: 5:12 AM
+ */
+
+namespace Ayimdomnic\GraphQl\Exceptions;
+
+
+
+use GraphQL\Error;
+use GraphQL\FormattedError;
+
+
+
+class ValidationError extends Error
+{
+    public $validator;
+
+    public function setValidator($validator)
+    {
+        $this->validator = $validator;
+
+        return $this;
+    }
+
+    public function getValidatorMessages()
+    {
+        return $this->validator ? $this->validator->messages():[];
+    }
+
+}
