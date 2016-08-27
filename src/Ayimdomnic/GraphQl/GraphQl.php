da7570a44737256bac61465fe99b8b96925445c6
Ayimdomnic <Ayimdomnic@statuscreative.co.ke>
2016-08-27 02:38:04 -0700

    More changes and formats on the main_file - this should check for interfaces and fromat errors

--- src/Ayimdomnic/GraphQl/GraphQl.php
+++ src/Ayimdomnic/GraphQl/GraphQl.php
@@ -18,7 +18,7 @@ use Ayimdomnic\GraphQl\Exceptions\ValidationError;
 class GraphQl
 {
     protected $app;
-    protected $instance;
+//    protected $instance;
 
     protected $mutations =[];
     protected $queries =[];
@@ -29,7 +29,7 @@ class GraphQl
     public function __construct($app, $instance)
     {
         $this->app = $app;
-        $this->instance = $instance;
+//        $this->instance = $instance;
     }
 
     public function schema()
@@ -75,7 +75,7 @@ class GraphQl
 
         $typeFields = [];
 
-        foreach($fields as $key => $field) {
+        foreach ($fields as $key => $field) {
 
             if (is_string($field)) {
                 $typeFields[$key] = app($field)->toArray();
@@ -85,7 +85,111 @@ class GraphQl
         }
 
         return new ObjectType(array_merge([
-            'fields' => typefields
+            'fields' => typeFields
         ], $options));
     }
+
+    public function query($query, $params = [])
+    {
+        $executionResult = $this->queryAndReturnResult($query, $params);
+
+        if (!empty($executionResult->errors))
+        {
+            $errorFormatter = config('graphql.error_formatter', ['\Folklore\GraphQL', 'formatError']);
+
+            return [
+                'data' => $executionResult->data,
+                'errors' => array_map($errorFormatter, $executionResult->errors)
+            ];
+        }
+        else
+        {
+            return [
+                'data' => $executionResult->data
+            ];
+        }
+    }
+
+        public function queryAndReturnResult($query, $params = [])
+    {
+        $schema = $this->schema();
+        $result = GraphQLBase::executeAndReturnResult($schema, $query, null, $params);
+        return $result;
+    }
+
+        public function addMutation($name, $mutator)
+    {
+        $this->mutations[$name] = $mutator;
+    }
+
+        public function addQuery($name, $query)
+    {
+        $this->queries[$name] = $query;
+    }
+
+        public function addType($class, $name = null)
+    {
+        if(!$name)
+        {
+            $type = is_object($class) ? $class:app($class);
+            $name = $type->name;
+        }
+
+        $this->types[$name] = $class;
+    }
+
+        public function type($name, $fresh = false)
+    {
+        if(!isset($this->types[$name]))
+        {
+            throw new \Exception('Type '.$name.' not found.');
+        }
+
+        if(!$fresh && isset($this->typesInstances[$name]))
+        {
+            return $this->typesInstances[$name];
+        }
+
+        $type = $this->types[$name];
+        if(!is_object($type))
+        {
+            $type = app($type);
+        }
+
+        $instance = $type->toType();
+        $this->typesInstances[$name] = $instance;
+
+        #Find available intefaces for the object
+        if($type->interfaces)
+        {
+            InterfaceType::addImplementationToInterfaces($instance);
+        }
+
+        return $instance;
+    }
+
+    public static function formatError(Error $e)
+    {
+        $error = [
+            'message' => $e->getMessage()
+        ];
+
+        $locations = $e->getLocations();
+        if(!empty($locations))
+        {
+            $error['locations'] = array_map(function($loc)
+            {
+                return $loc->toArray();
+            }, $locations);
+        }
+
+        $previous = $e->getPrevious();
+        if($previous && $previous instanceof ValidationError)
+        {
+            $error['validation'] = $previous->getValidatorMessages();
+        }
+
+        return $error;
+    }
+
 }
