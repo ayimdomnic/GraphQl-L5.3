375f49dd31577e3aff5c52a84a660c84d0a1b7cb
Ayimdomnic <Ayimdomnic@statuscreative.co.ke>
2016-08-27 03:25:25 -0700

    GraphQl Facade already in

---
+++ src/Ayimdomnic/GraphQl/Helper/Facades/GraphQl.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ayimdomnic
+ * Date: 8/27/2016
+ * Time: 3:24 AM
+ */
+
+namespace Ayimdomnic\GraphQl\Helper\Facades;
+
+
+use Illuminate\Support\Facades\Facade;
+
+class GraphQl extends Facade
+{
+
+}
---
+++ src/Ayimdomnic/GraphQl/Helper/Query.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ayimdomnic
+ * Date: 8/27/2016
+ * Time: 3:22 AM
+ */
+
+namespace Ayimdomnic\GraphQl\Helper;
+
+
+class Query
+{
+
+}
