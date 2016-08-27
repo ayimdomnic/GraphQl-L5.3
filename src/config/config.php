5d636137d3aac11a38f9b2d97872d949ced910d6
Ayimdomnic <Ayimdomnic@statuscreative.co.ke>
2016-08-27 01:45:08 -0700

    Initial commit, stubs and templates

---
+++ src/config/config.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ayimdomnic
+ * Date: 8/27/2016
+ * Time: 12:51 AM
+ */
+
+return [
+
+    'prefix' => 'graphql',
+
+    'routes' => '/',
+
+    'controllers' => 'Ayimdomnic\GraphQl\GraphQlController@inquire',
+
+    'middleware' => [],
+
+    'schema' => [
+        'query' => [
+
+        ],
+        'mutation' => [
+
+        ]
+    ],
+
+
+    'types' => [
+
+    ],
+
+
+    'error_formatter' => ['\Ayimdomnic\GraphQL\GraphQL', 'formatError']
+
+
+];
