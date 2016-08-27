08d48f8bee62ade0eca8b337c502a2c87720f684
Ayimdomnic <Ayimdomnic@statuscreative.co.ke>
2016-08-27 03:39:28 -0700

    Fixes on the GraphQlController

--- src/Ayimdomnic/GraphQl/GraphQlController.php
+++ src/Ayimdomnic/GraphQl/GraphQlController.php
@@ -7,5 +7,16 @@ use Illuminate\Http\Request;
 
 class GraphQlController extends Controller
 {
-    
+    public function inquire(Request $request)
+    {
+        $query = $request->get('query');
+        $params = $request->get('params');
+
+        if(is_string($params))
+        {
+            $params = json_decode($params, true);
+        }
+
+        return app('graphql')->query($query, $params);
+    }
 }
