c607dbb5c8debbe9271be216477c060772da1fe2
Ayimdomnic <Ayimdomnic@statuscreative.co.ke>
2016-08-27 02:45:04 -0700

    GraphQlServiceProvider, registration and publishing, normal laravel package ways

---
+++ src/Ayimdomnic/GraphQl/GraphQlServiceProvider.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Ayimdomnic\GraphQl;
+
+use Illuminate\Support\ServiceProvider;
+
+
+class GraphQlServiceProvider extends SeviceProvider
+{
+
+    public function boot()
+    {
+        $this->bootPublishes();
+
+        $this->bootTypes();
+
+        if(config('graphql.routes'))
+        {
+            include __DIR__.'/routes.php';
+        }
+    }
+
+    protected function bootPublishes()
+    {
+        $configPath = __DIR__.'/../../config';
+
+        $this->mergeConfigFrom($configPath.'/config.php', 'graphql');
+
+        $this->publishes([
+            $configPath.'/config.php' => config_path('graphql.php'),
+        ], 'config');
+    }
+
+
+    protected function bootTypes()
+    {
+        $configTypes = config('graphql.types');
+        foreach($configTypes as $name => $type)
+        {
+            if(is_numeric($name))
+            {
+                $this->app['graphql']->addType($type);
+            }
+            else
+            {
+                $this->app['graphql']->addType($type, $name);
+            }
+        }
+    }
+
+    public function registerGraphQL()
+    {
+        $this->app->singleton('graphql', function($app)
+        {
+            return new GraphQL($app);
+        });
+    }
+
+    public function register()
+    {
+        $this->registerGraphQL();
+    }
+
+}
