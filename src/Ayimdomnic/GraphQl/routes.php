3747a997661ebc7ed32773d58d73ae1024439d3a
Ayimdomnic <Ayimdomnic@statuscreative.co.ke>
2016-08-27 02:52:24 -0700

    Routes for queries and mutations, todo:controllers and the query Method

---
+++ src/Ayimdomnic/GraphQl/routes.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Http\Request;
+
+
+Route::group(array(
+    'prefix' => config('graphql.prefix'),
+    'middleware' => config('graphql.middleware', [])
+), function()
+{
+    //Routes for GraphQl
+    $routes = config('graphql.routes');
+    $queryRoute = null;
+    $mutationRoute = null;
+    if(is_array($routes))
+    {
+        $queryRoute = array_get($routes, 'query', null);
+        $mutationRoute = array_get($routes, 'mutation', null);
+    }
+    else
+    {
+        $queryRoute = $routes;
+        $mutationRoute = $routes;
+    }
+
+    //Controllers
+    $controllers = config('graphql.controllers', '\Ayimdomnic\GraphQl\GraphQlController@query');
+    $queryController = null;
+    $mutationController = null;
+    if(is_array($controllers))
+    {
+        $queryController = array_get($controllers, 'query', null);
+        $mutationController = array_get($controllers, 'mutation', null);
+    }
+    else
+    {
+        $queryController = $controllers;
+        $mutationController = $controllers;
+    }
+
+    // this is the query route
+    if($queryRoute)
+    {
+        Route::get($queryRoute, array(
+            'as' => 'graphql.query',
+            'uses' => $queryController
+        ));
+    }
+
+    if($mutationRoute)
+    {
+        //this is the route for mutation
+        Route::post($mutationRoute, array(
+            'as' => 'graphql.mutation',
+            'uses' => $mutationController
+        ));
+    }
+});
